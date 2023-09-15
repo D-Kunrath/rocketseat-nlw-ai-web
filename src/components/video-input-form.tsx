93486514c1d53e150f6d786d1b8683c2a3eccc6e
Daniel Kunrath Albanez <dkunrath@gmail.com>
2023-09-14 21:15:37 -0300

    feat: video transfer connected to api

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -6,9 +6,21 @@ import { Button } from "./ui/button";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from '@ffmpeg/util'
+import { api } from "@/lib/axios";
+
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success';
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!'
+}
 
 export function VideoInputForm () {
   const [ videoFile, setVideoFile ] = useState<File | null>(null);
+  const [ status, setStatus ] = useState<Status>('waiting');
+
   const promptInputRef = useRef<HTMLTextAreaElement>(null);
 
   function handleFileSelected(e: ChangeEvent<HTMLInputElement>) {
@@ -46,10 +58,8 @@ export function VideoInputForm () {
       'output.mp3'
     ])
 
-    console.log('reading...')
     const data = await ffmpeg.readFile('output.mp3');
     
-    console.log('creating file...')
     const audioFileBlob = new Blob([data], { type: 'audio/mpeg' });
     const audioFile = new File([audioFileBlob], 'audio.mp3', { type: 'audio/mpeg' });
 
@@ -65,9 +75,27 @@ export function VideoInputForm () {
 
     if (!videoFile) return;
 
+    setStatus('converting');
+
     const audioFile = await convertVideoToAudio(videoFile);
 
-    console.log(audioFile, prompt);
+    const data = new FormData();
+
+    data.append('file', audioFile);
+
+    setStatus('uploading');
+
+    const response = await api.post('/videos', data);
+
+    const videoId = response.data.video.id;
+
+    setStatus('generating');
+
+    await api.post(`/videos/${videoId}/transcription`, {
+      prompt
+    });
+
+    setStatus('success');
   }
 
   const previewURL = useMemo(() => {
@@ -78,37 +106,47 @@ export function VideoInputForm () {
 
   return (
     <form className="space-y-6" onSubmit={handleUploadVideo}>
-    <label
-      htmlFor="video"
-      className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-    >
-      {previewURL ? (
-        <video src={previewURL} controls={false} className="pointer-events-none absolute inset-0" />
-      ) : (
-        <>
-          <FileVideo className="w-4 h-4" />
-          Selecione vídeo
-        </>
-      )}
-    </label>
-    <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelected} />
+      <label
+        htmlFor="video"
+        className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+      >
+        {previewURL ? (
+          <video src={previewURL} controls={false} className="pointer-events-none absolute inset-0" />
+        ) : (
+          <>
+            <FileVideo className="w-4 h-4" />
+            Selecione vídeo
+          </>
+        )}
+      </label>
+      <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelected} />
 
-    <Separator />
+      <Separator />
 
-    <div className="space-y-2">
-      <Label htmlFor="trancription_prompt">Promp de transcrição</Label>
-      <Textarea
-        ref={promptInputRef}
-        id="transciption_prompt"
-        className="h-20 leading-relaxed resize-none"
-        placeholder="Inclua palavras-chave mencionadas no video spearadas por vírgula (,)"
-      />
-    </div>
+      <div className="space-y-2">
+        <Label htmlFor="trancription_prompt">Promp de transcrição</Label>
+        <Textarea
+          ref={promptInputRef}
+          id="transciption_prompt"
+          className="h-20 leading-relaxed resize-none"
+          placeholder="Inclua palavras-chave mencionadas no video spearadas por vírgula (,)"
+          disabled={status !== 'waiting'}
+        />
+      </div>
 
-    <Button type="submit" className="w-full">
-      Carregar vídeo
-      <Upload className="w-4 h-4 ml-2"/>
-    </Button>
-  </form>
+      <Button
+        data-success={status === 'success'}
+        type="submit"
+        className="w-full data-[success=true]:bg-emerald-400"
+        disabled={status !== 'waiting'}
+      >
+        {status === 'waiting' ? (
+          <>
+            Carregar vídeo
+            <Upload className="w-4 h-4 ml-2"/>
+          </>
+        ) : statusMessages[status] }
+      </Button>
+    </form>
   )
 }
