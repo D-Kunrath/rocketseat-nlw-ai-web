63a7f3456c8a8d8fbf122fe29fa4b3714ef44214
Daniel Kunrath Albanez <dkunrath@gmail.com>
2023-09-14 21:39:03 -0300

    feat: join form data after video upload

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,16 @@ import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
 
 export function App() {
+  const [ temperature, setTemperature ] = useState(0.5);
+  const [ videoId, setVideoId ] = useState<string | null>(null)
+
+  function handlePromptSelected(template: string) {
+    console.log(template);
+  }
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -42,26 +50,14 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator />
 
           <form className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">
-                    Título do Youtube
-                  </SelectItem>
-                  <SelectItem value="description">
-                    Descrição do Youtube
-                  </SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={handlePromptSelected} />
             </div>
 
             <Separator />
@@ -91,6 +87,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
                 Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
